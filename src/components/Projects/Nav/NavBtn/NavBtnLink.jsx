d40d49607d27b5582c29585eb3f168b3b1c46ec9
Andatax <abraham.mendez1011@gmail.com>
2024-02-01 18:58:35 -0600

    improved animations and media queries

--- src/components/Projects/Nav/NavBtn/NavBtnLink.jsx
+++ src/components/Projects/Nav/NavBtn/NavBtnLink.jsx
@@ -1,6 +1,6 @@
 export default function NavBtnLink({ btnText, btnId, btnLink }) {
 	return (
-		<li className="mx-10 mt-2">
+		<li className="mx-10 mt-2 hover:underline">
 			<a href={btnLink} target="_blank" id={btnId} className="text-center">
 				{btnText}
 			</a>
